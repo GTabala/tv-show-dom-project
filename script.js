d23b5ab5cd94d11f37f71093e5a1771aa1769e79
Gennady Tabala <gtabala@gmail.com>
2020-08-18 02:11:21 +0100

    Level 100

--- script.js
+++ script.js
@@ -3,10 +3,29 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
+function getEpisodeNumber(episode){
 
+}
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((element) => {
+    let episodeContainer = document.createElement("div");
+    let titleDiv = document.createElement("div");
+    titleDiv.className = "titleEp";
+    let epImage = document.createElement("img");
+    episodeContainer.className = "episode";
+    console.log(element);
+    titleDiv.innerText = `${element.name} - S${("0" + element.season ).slice(-2)}E${("0" + element.number ).slice(-2)}`;
+    epImage.src = element.image.medium;
+    epImage.className = "epImg";
+
+    episodeContainer.innerHTML = element.summary;
+    rootElem.appendChild(episodeContainer);
+
+    episodeContainer.insertBefore(epImage, episodeContainer.firstChild);
+    episodeContainer.insertBefore(titleDiv, episodeContainer.firstChild);
+  });
 }
 
 window.onload = setup;
