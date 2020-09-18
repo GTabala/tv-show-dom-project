a3e779130650b174c1cf95fb64ebfaa304cb7fe4
Gennady Tabala <gtabala@gmail.com>
2020-09-18 18:49:50 +0100

    Start level 500

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-function setup() {
-  const allShows = getAllShows();
-  allShows.sort((a,b) =>{
-    if(a.name.toLowerCase() > b.name.toLowerCase()){
-      return 1;
-    }
-     if(a.name.toLowerCase() < b.name.toLowerCase()){
-      return -1;
-    }
-    return 0;
-  }).forEach((item) => {
-    let option = document.createElement("option");
-    option.value = item.id;
-    option.innerText = item.name;
-    chooseAnShow.appendChild(option);
-  });
+let showSelector = document.createElement("select");
+showSelector.id = "showSelector";
+showSelector.name = "chooseShow";
+showSelector.style.height = "30px";
 
+document.getElementById("root").style.display = "none";
+
+let showsContainer = document.createElement("div");
+showsContainer.id = "showsContainer";
+document.body.appendChild(showsContainer);
+
+let navShow = document.createElement("nav");
+navShow.id = "navShow";
+navShow.style.display = "flex";
+navShow.style.alignItems = "center";
+showsContainer.appendChild(navShow);
+navShow.appendChild(showSelector);
+let optionAllShow = document.createElement("option");
+optionAllShow.value = 0;
+optionAllShow.innerText = "All shows";
+showSelector.appendChild(optionAllShow);
+function setup() {
+  let allShows = getAllShows().filter((i) => i.id != 1127);
+  allShows.sort((a, b) => {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return 1;
+      }
+      if (a.name.toLowerCase() < b.name.toLowerCase()) {
+        return -1;
+      }
+      return 0;
+    })
+    .forEach((item) => {
+      let option = document.createElement("option");
+      option.value = item.id;
+      option.innerText = item.name;
+      chooseAShow.appendChild(option);
+      
+      showSelector.appendChild(option.cloneNode(true));
+    });
+makePageForShows(allShows);
   //loadShow(allShows[0].id); // Not useful on the level 500
 }
+
+function makePageForShows(showList){
+showList.forEach(showObject =>{
+  let showContainer = document.createElement("div");
+  showContainer.className = "show";
+  showContainer.innerHTML = `<h1>${showObject.name}</h1><br><img src = "${showObject.image.medium}">`;
+  showsContainer.appendChild(showContainer);
+});
+addFooter();
+};
 
 function loadShow(id) {
   let removeOptions = chooseAnEpisode.querySelectorAll("option");
@@ -27,7 +61,7 @@ function loadShow(id) {
   search.value = "";
 
   let removeEpisodes = rootElem.querySelectorAll(".episode");
- 
+
   removeEpisodes.forEach((item) => rootElem.removeChild(item));
 
   fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
@@ -46,11 +80,11 @@ nav.id = "navBar";
 nav.style.display = "flex";
 nav.style.alignItems = "center";
 
-let chooseAnShow = document.createElement("select");
-chooseAnShow.id = "chooseAnShow";
-chooseAnShow.name = "choose";
-chooseAnShow.style.height = "30px";
-nav.appendChild(chooseAnShow);
+let chooseAShow = document.createElement("select");
+chooseAShow.id = "chooseAShow";
+chooseAShow.name = "choose";
+chooseAShow.style.height = "30px";
+nav.appendChild(chooseAShow);
 
 let chooseAnEpisode = document.createElement("select");
 chooseAnEpisode.id = "chooseAnEpisode";
@@ -93,13 +127,13 @@ function makePageForEpisodes(episodeList) {
     helpfulArray.push({
       episode: episodeContainer,
       name: element.name.toLowerCase(),
-      summary: element.summary ? element.summary.toLowerCase():"",
+      summary: element.summary ? element.summary.toLowerCase() : "",
       fullNumber: episodeFullNumber,
     });
     titleDiv.innerText = `${element.name} - ${episodeFullNumber}`;
 
-    episodeContainer.innerHTML = element.summary ? element.summary:``;
-    
+    episodeContainer.innerHTML = element.summary ? element.summary : ``;
+
     rootElem.appendChild(episodeContainer);
 
     let episodeOption = document.createElement("option");
@@ -122,12 +156,14 @@ function makePageForEpisodes(episodeList) {
   displayedEpisodesP.innerText = `Displaying ${displayedEpisodes}/${numberOfAllEpisodes} episodes`;
 }
 
-let footer = document.createElement("footer");
-footer.style.textAlign = "right";
-footer.style.paddingRight = "20px";
-footer.innerHTML =
-  'Data provided by <a href = "https://www.tvmaze.com/" target = "_blank" >www.tvmaze.com</a>';
-document.body.appendChild(footer);
+function addFooter() {
+  let footer = document.createElement("footer");
+  footer.style.textAlign = "right";
+  footer.style.paddingRight = "20px";
+  footer.innerHTML =
+    'Data provided by <a href = "https://www.tvmaze.com/" target = "_blank" >www.tvmaze.com</a>';
+  document.body.appendChild(footer);
+}
 
 function searchEpisodes() {
   chooseAnEpisode.value = "All episodes";
@@ -151,10 +187,9 @@ search.onkeyup = searchEpisodes;
 chooseAnEpisode.onchange = (event) => {
   search.value = "";
   if (event.target.value === "All episodes") {
-    
     searchEpisodes();
   } else {
-    helpfulArray.forEach(element => {
+    helpfulArray.forEach((element) => {
       if (element.fullNumber === event.target.value) {
         element.episode.classList.remove("hidden");
         displayedEpisodes++;
@@ -165,13 +200,14 @@ chooseAnEpisode.onchange = (event) => {
   }
 };
 document.addEventListener("click", (event) => {
-  if(event.target.parentNode.className === "episode") {loadShow(event.target.parentNode.firstChild.innerText);
-     chooseAnShow.value = 82;}
-  else if (event.target.className === "episode") {loadShow(event.target.firstChild.innerText);
-    chooseAnShow.value = 82;}
-  
+  if (event.target.parentNode.className === "episode") {
+    loadShow(event.target.parentNode.firstChild.innerText);
+    chooseAShow.value = 82;
+  } else if (event.target.className === "episode") {
+    loadShow(event.target.firstChild.innerText);
+    chooseAShow.value = 82;
+  }
 });
-chooseAnShow.onchange = (event) => loadShow(event.target.value);
-
+chooseAShow.onchange = (event) => loadShow(event.target.value);
 
 window.onload = setup;
